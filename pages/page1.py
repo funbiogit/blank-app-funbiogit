0955bdece88ff26affe28fcea3fbed92204d8f95
funbiogit <funbio@naver.com>
2025-07-21 11:29:11 +0000

    폰트 폴더 경로를 최상단으로부터 읽기 시작하는 것으로 수정

--- pages/page1.py
+++ pages/page1.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 import numpy as np
 
-# 한글 폰트 설정 (맑은 고딕)
-plt.rcParams['font.family'] = 'Malgun Gothic'
+# NanumGothic 폰트 경로 지정
+font_path = "./fonts/NanumGothic-Regular.ttf"
+fontprop = fm.FontProperties(fname=font_path)
+
+# matplotlib에 폰트 등록 및 기본 폰트로 설정
+plt.rcParams['font.family'] = fontprop.get_name()
 plt.rcParams['axes.unicode_minus'] = False  # 마이너스 깨짐 방지
 
 st.title("Matplotlib 데이터 시각화 예시")
@@ -14,11 +19,11 @@ y = np.random.randint(10, 100, size=10)
 
 # 그래프 그리기
 fig, ax = plt.subplots()
-ax.plot(x, y, marker='o', label='데이터 값')
-ax.set_title('샘플 데이터 그래프 (한글 제목)')
-ax.set_xlabel('X축 (한글)')
-ax.set_ylabel('Y축 (한글)')
-ax.legend()
+ax.plot(x, y, marker='o', label='데이터 값', color='blue')
+ax.set_title('샘플 데이터 그래프 (한글 제목)', fontproperties=fontprop)
+ax.set_xlabel('X축 (한글)', fontproperties=fontprop)
+ax.set_ylabel('Y축 (한글)', fontproperties=fontprop)
+ax.legend(prop=fontprop)
 
 # Streamlit에 그래프 표시
 st.pyplot(fig)
